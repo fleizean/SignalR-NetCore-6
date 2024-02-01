87d2d8e6e0f251d4289473ec110a4c2f3a55a11d
Enes <nsyagz@gmail.com>
2024-02-01 21:03:30 +0300

    Layers added and Context added

---
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.Concrete
+{
+	public class SignalRContext:DbContext
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost;database=SealingsProjectDBNew;User=SA; Password=reallyStrongPwd123"); // bağlantıyı tutan yapı
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
+
