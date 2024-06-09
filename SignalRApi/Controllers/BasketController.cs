003a358a0c4e45e65af6efc92e6bdd4e8492aa7f
Enes <nsyagz@gmail.com>
2024-06-09 19:08:38 +0300

    chore: Update cart link in navbar to point to /Basket/Index

--- SignalRApi/Controllers/BasketController.cs
+++ SignalRApi/Controllers/BasketController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
 using SignalR.DTOLayer.BasketDto;
+using SignalR.EntityLayer.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,6 +31,33 @@ namespace SignalRApi.Controllers
         {
             var values = _mapper.Map<List<ResultBasketWithProductDto>>(_basketService.TGetBaskeyByMenuTableNumber(id));
             return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBasket(CreateBasketDto createBasketDto)
+        {
+            using var context = new SignalRContext();
+            if (createBasketDto != null)
+            {
+                _basketService.TAdd(new Basket()
+                {
+                    ProductID = createBasketDto.ProductID,
+                    Count = 1,
+                    MenuTableID = 1,
+                    Price = context.Products.Where(x => x.ProductID == createBasketDto.ProductID).Select(a => a.Price).FirstOrDefault(),
+                    TotalPrice = 0,
+                    Status = true
+                });
+                return Ok("Basket added successfuly");
+            }
+            return NotFound("Basket not added successfuly");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBasketById(int id)
+        {
+            var value = _basketService.TGetById(id);
+            return Ok(value);
         }
     }
 }
--- SignalRWebUI/Controllers/BasketController.cs
+++ SignalRWebUI/Controllers/BasketController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -21,17 +22,18 @@ namespace SignalRWebUI.Controllers
         // GET: /<controller>/
         public async Task<IActionResult> Index()
         {
-            int id = 1;
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7083/api/Basket/GetBasketByMenuTableID?id={id}");
+            var responseMessage = await client.GetAsync($"https://localhost:7083/api/Basket/GetBasketByMenuTableID?id={1}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultBasketWithProductDto>>(jsonData);
+
                 return View(values);
             }
             return View();
         }
+        
     }
 }
 
